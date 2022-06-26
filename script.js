29bbbc0ee01a81010d77531141dc60b990f33ec0
Juan Gustavo <juangustavomonteiro@gmail.com>
2022-06-25 23:50:33 -0300

    Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function  minimizar () {
+   let menu = document.getElementById('menu-area');
+   if (menu.style.width=="200px"){
+      menu.style.width = "0px"
+   }else {
+      menu.style.width = "200px"
+   }
+}
